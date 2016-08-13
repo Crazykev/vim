5f48bece6b06339488810747e0494f144be8b691
Crazykev <crazykev@zju.edu.cn>
2016-08-14 02:24:07 +0800

    add golang environment setup script
    Signed-off-by: Crazykev <crazykev@zju.edu.cn>

--- setup.sh
+++ setup.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 if which apt-get >/dev/null; then
-	sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
+	sudo apt-get install -y wget vim vim-gnome ctags xclip astyle python-setuptools python-dev git
 elif which yum >/dev/null; then
-	sudo yum install -y gcc vim git ctags xclip astyle python-setuptools python-devel	
+	sudo yum install -y wget gcc vim git ctags xclip astyle python-setuptools python-devel	
 fi
 
 ##Add HomeBrew support on  Mac OS
 if which brew >/dev/null;then
     echo "You are using HomeBrew tool"
     brew install vim ctags git astyle
+fi
+
+echo "Configuring golang environment, version 1.6.3..."
+if which go > /dev/null; then
+    echo "Retain original golang Environment"
+    echo `go version`
+else
+    wget https://storage.googleapis.com/golang/go1.6.3.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go1.6.3.linux-amd64.tar.gz
+    cat >>~/.bashrc <<EOF
+    export GOPATH=\$GOPATH:\$HOME/go-project
+    export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin/
+EOF
 fi
 
 sudo easy_install -ZU autopep8 
@@ -27,3 +40,4 @@ echo "请耐心等待" >> ma6174
 vim ma6174 -c "BundleInstall" -c "q" -c "q"
 rm ma6174
 echo "安装完成"
+
